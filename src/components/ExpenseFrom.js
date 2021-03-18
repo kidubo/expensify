5ebf643d984d30191cb4e55697e7641bf3d0599b
kidubo <innocentleonard007@gmail.com>
2021-03-18 05:43:40 +0300

    regular expresion for numbers

--- src/components/ExpenseFrom.js
+++ src/components/ExpenseFrom.js
@@ -1,10 +1,58 @@
 import React from 'react';
 
 export default class ExpenseForm extends React.Component {
+    state = {
+        description: '',
+        note:'',
+        amount: ''
+    };
+
+    onDescriptionChange = (e) => {
+        const description = e.target.value
+        this.setState(()=>({
+            description
+        }))
+    }
+
+    onNoteChange =(e) => {
+        const note = e.target.value
+        this.setState(()=> ({note}))
+    }
+
+    onAmountChange = (e) => {
+        const amount = e.target.value
+        if(amount.match(/^\d*(\.\d{0,2})?$/)){
+            this.setState(()=>({amount}))
+        }
+    }
+
     render(){
         return (
             <div>
-              Expense Form
+            <form>
+              <input type = "text"
+                    placeholder="descripition"
+                    autoFocus
+                    value = {this.state.description} 
+                    onChange = {this.onDescriptionChange}
+               />
+
+               <input type="number"
+                      placeholder="amount"
+                      value = {this.state.amount}
+                      onChange ={this.onAmountChange}
+
+                />
+
+                <textarea
+                  placeholder = "Add a note for your expense (optional)"
+                  value= {this.state.note}
+                  onChange= {this.onNoteChange}
+                >
+                </textarea>
+                <button> Add Expense</button>
+            
+            </form>
             </div>
         )
     }
