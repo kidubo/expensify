d079384e583cffabf655075b181c539bbf2a4c6d
kidubo <innocentleonard0007@gmail.com>
2021-06-11 15:00:44 +0300

    using npx react-codemod rename-unsafe-lifecycles

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -3,12 +3,13 @@ import AddExpensePage from '../components/AddExpensePage';
 import EditExpense from '../components/EditExpensePage';
 import ExpenseDashboard from '../components/ExpenseDashboard';
 import Help from '../components/Help';
-import Header from '../components/Header';
 import NotFound from '../components/NotFound';
 import { createBrowserHistory } from 'history';
 import { Router ,Route , Switch,} from 'react-router-dom';
 import  LoginPage from '../components/LoginPage';
+import Header from '../components/Header';
 import PrivateRoute from './PrivateRoute';
+
 
 export let history = createBrowserHistory();
 
@@ -19,9 +20,9 @@ const AppRouter = () => {
         <Header />
         <Switch>
             <Route path="/" component={LoginPage} exact = {true} />
-            <PrivateRoute path="/dashboard" component={ExpenseDashboard} />
-            <PrivateRoute path ="/create" component = {AddExpensePage} />
-            <PrivateRoute path = "/edit/:id" component = {EditExpense} />
+            <Route path="/dashboard" component={ExpenseDashboard} />
+            <Route path ="/create" component = {AddExpensePage} />
+            <Route path = "/edit/:id" component = {EditExpense} />
             <Route path = "/help" component = {Help} />
             <Route component = {NotFound} />
         </Switch>
